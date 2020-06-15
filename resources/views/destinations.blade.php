e59fff853b973a391f345bd968228925e3bbff6b
Ks5810 <e40keisuke@gmail.com>
2020-06-14 20:07:43 -0400

    6/14 moved list on top

--- resources/views/destinations.blade.php
+++ resources/views/destinations.blade.php
@@ -4,6 +4,58 @@
     <div class="container" style="padding-top: 4rem">
         <div class="row justify-content-center">
             <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ $username }}'s Destinations
+                    </div>
+                    <div class="list-group">
+                        @foreach( $destinations as $key => $destination )
+                            <div class="list-group-item">
+                                <div class="d-flex w-100 justify-content-between">
+                                    <p><b>{{ $key+1 }}. {{
+                                    $destination->name
+                                    }}</b></p>
+                                    <form
+                                            action="/destinations/{{ $destination->id }}"
+                                            method="post"
+                                    >
+                                        {{ method_field('delete') }}
+                                        {{ csrf_field() }}
+                                        <button
+                                                class="btn btn-sm btn-danger btn-remove"
+                                                type="submit"
+                                        >
+                                            Remove
+                                        </button>
+                                    </form>
+                                </div>
+                                {{--                                <div class="px-2 py-1">--}}
+                                {{--                                    <p class="d-flex w-50--}}
+                                {{--                                justify-content-between flex-row mb-1">--}}
+                                {{--                                        <b>latitude:</b> {{ $destination->lat }}--}}
+                                {{--                                    </p>--}}
+                                {{--                                    <p class="d-flex w-50--}}
+                                {{--                                justify-content-between flex-row mb-1">--}}
+                                {{--                                        <b>longitude:</b> {{ $destination-> lng }}--}}
+                                {{--                                    </p>--}}
+                                {{--                                </div>--}}
+                            </div>
+                        @endforeach
+                    </div>
+                    <div class="container">
+                        <div id="map"
+                             center_lat='{{ $center_lat }}'
+                             center_lng='{{ $center_lng }}'
+                             destinations='{{ $destinations }}'>
+                            <script src="../js/app.js"></script>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
                 <div class="card justify-content-center">
                     <div class="card-header">
                         Add New Destination by Address
@@ -148,58 +200,6 @@
                                 </button>
                             </div>
                         </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ $username }}'s Destinations
-                    </div>
-                    <div class="list-group">
-                        @foreach( $destinations as $key => $destination )
-                            <div class="list-group-item">
-                                <div class="d-flex w-100 justify-content-between">
-                                    <p><b>{{ $key+1 }}. {{
-                                    $destination->name
-                                    }}</b></p>
-                                    <form
-                                            action="/destinations/{{ $destination->id }}"
-                                            method="post"
-                                    >
-                                        {{ method_field('delete') }}
-                                        {{ csrf_field() }}
-                                        <button
-                                                class="btn btn-sm btn-danger btn-remove"
-                                                type="submit"
-                                        >
-                                            Remove
-                                        </button>
-                                    </form>
-                                </div>
-{{--                                <div class="px-2 py-1">--}}
-{{--                                    <p class="d-flex w-50--}}
-{{--                                justify-content-between flex-row mb-1">--}}
-{{--                                        <b>latitude:</b> {{ $destination->lat }}--}}
-{{--                                    </p>--}}
-{{--                                    <p class="d-flex w-50--}}
-{{--                                justify-content-between flex-row mb-1">--}}
-{{--                                        <b>longitude:</b> {{ $destination-> lng }}--}}
-{{--                                    </p>--}}
-{{--                                </div>--}}
-                            </div>
-                        @endforeach
-                    </div>
-                    <div class="container">
-                        <div id="map"
-                             center_lat='{{ $center_lat }}'
-                             center_lng='{{ $center_lng }}'
-                             destinations='{{ $destinations }}'>
-                            <script src="../js/app.js"></script>
-                        </div>
                     </div>
                 </div>
             </div>
